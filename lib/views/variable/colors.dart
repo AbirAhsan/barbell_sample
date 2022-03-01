6a3f59accf28536335a6e49d2130bba59d69fdba
Abir Ahsan <abirahsan122@gmail.com>
2022-03-01 15:27:45 +0600

    add some color plates

---
+++ lib/views/variable/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color limeblack = Color.fromARGB(255, 18, 19, 19);
+const Color offWhite = Color.fromARGB(255, 229, 229, 231);
+const Color grey = Color.fromARGB(255, 185, 185, 187);
+const Color white = Color.fromARGB(255, 255, 255, 255);
