b00da99b351d6a6f15f4b46ef23614afa3112b85
Abir Ahsan <abirahsan122@gmail.com>
2022-03-01 15:48:52 +0600

    add product screen appbar outline

--- lib/views/products_view/product_main_screen.dart
+++ lib/views/products_view/product_main_screen.dart
@@ -1,4 +1,7 @@
+import 'package:barbell/views/variable/colors.dart';
+import 'package:barbell/views/variable/text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProductMainScreen extends StatelessWidget {
   const ProductMainScreen({Key? key}) : super(key: key);
@@ -11,14 +14,43 @@ class ProductMainScreen extends StatelessWidget {
       body: SizedBox(
         height: _height,
         width: _height,
-        child: Column(
+        child: Stack(
           children: [
-            Container(
-              width: _width,
-              child: Row(
-                children: [],
+            AppBar(
+              toolbarHeight: 120,
+              backgroundColor: limeblack,
+              title: Text(
+                AppLocalizations.of(context)!.productScreenTitle,
+                style: titleBoldStyleWithWhite,
               ),
-            )
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.apple,
+                      color: white,
+                    )),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: _height - 140,
+                width: _width,
+                decoration: BoxDecoration(
+                  color: offWhite,
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(
+                      30.0,
+                    ),
+                    topRight: Radius.circular(
+                      30.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/views/variable/text_style.dart
@@ -0,0 +1,44 @@
+import 'package:barbell/views/variable/colors.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle titleBoldStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
+const TextStyle titleBoldStyleWithWhite = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+  color: white,
+);
+
+const TextStyle titleRegularStyle = TextStyle(
+  fontSize: 20,
+);
+const TextStyle titleRegularStyleWithWhite = TextStyle(
+  fontSize: 20,
+  color: white,
+);
+const TextStyle normalBoldStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+const TextStyle normalBoldStyleWithWhite = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  color: white,
+);
+const TextStyle normalRegularStyle = TextStyle(
+  fontSize: 16,
+);
+
+const TextStyle normalRegularStyleWithWhite = TextStyle(
+  fontSize: 16,
+  color: white,
+);
+const TextStyle smallRegularStyle = TextStyle(
+  fontSize: 14,
+);
+const TextStyle smallRegularStyleWithWhite = TextStyle(
+  fontSize: 14,
+  color: white,
+);
